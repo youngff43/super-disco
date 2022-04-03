92c6f21a34e4692ef621e2915a1c2fa8cfbf2dd5
youngff43 <youngff43@gmail.com>
2022-04-03 13:42:13 -0400

    added the save to localstorage

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,13 +3,13 @@ var todaysDateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").html(todaysDateTime);
 
 //function to save text//
-$(document).ready(function() {
+$(document).ready(function () {
 $(".saveButton").on("click", function () {
 var task = $(this).siblings(".description").val();
 var time = $(this).parent().attr("id");
 
 //adds to lcocal storage//
-localStorage.setItem(task,time);
+localStorage.setItem(time, task);
 })
 
 //function for number of hours//
@@ -18,7 +18,7 @@ function hourTracker() {
 
     //adding the loop//
     $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split(hour)[1]);
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
         //Checking the hour and adding the classes//
             //past//
@@ -42,4 +42,19 @@ function hourTracker() {
         }
     })
 }
+
+//Getting any information from local storage//
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+//Calling the function//
+hourTracker();
+
 })
